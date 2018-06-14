4a6ed137ed4f6185c5386c80509f48879700989d
byio <ben@benyang.io>
2018-06-14 16:13:31 +0800

    create services page

---
+++ src/pages/services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ServicesPage = () => (
+  <div>
+    <h1>here's what i can do for you</h1>
+    <p>Enim in tempor reprehenderit voluptate in voluptate Lorem ut consectetur aute sit. Ad voluptate Lorem sit aliquip nisi exercitation eiusmod id cillum qui sunt eu proident et dolore culpa laboris. Ad id ut sit amet ipsum id laboris. Et duis incididunt et dolore aute elit amet magna exercitation aliquip laboris fugiat.</p>
+    <p>Enim in tempor reprehenderit voluptate in voluptate Lorem ut consectetur aute sit. Ad voluptate Lorem sit aliquip nisi exercitation eiusmod id cillum qui sunt eu proident et dolore culpa laboris. Ad id ut sit amet ipsum id laboris. Et duis incididunt et dolore aute elit amet magna exercitation aliquip laboris fugiat.</p>
+  </div>
+);
+
+export default ServicesPage;
