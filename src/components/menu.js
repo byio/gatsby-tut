6c4027bcf2b77e837d44ce4effc454e1cb8445e9
byio <ben@benyang.io>
2018-06-17 06:10:25 +0800

    add link to /blog on menu component

--- src/components/menu.js
+++ src/components/menu.js
@@ -14,6 +14,7 @@ const Menu = () => (
       <li><Link to="/">home</Link></li>
       <li><Link to="/about">about</Link></li>
       <li><Link to="/services">services</Link></li>
+      <li><Link to="/blog">blog</Link></li>
     </ul>
   </div>
 );
